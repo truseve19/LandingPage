399b3674a10cd2181ca7e750990f231c7e40dd28
Raj kumar <rajkumar@redink.app>
2023-04-04 18:23:59 +0530

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import Footer from '../comps/Layout/Footer';
 import Nav from '../comps/Layout/Nav';
 import '../styles/globals.css';
@@ -5,6 +7,17 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E63L4H4JT9"
+        onLoad={() => {
+          window.dataLayer = window.dataLayer || []
+          function gtag() { dataLayer.push(arguments) }
+          gtag('js', new Date())
+
+          gtag('config', 'G-E63L4H4JT9')
+        }}
+      />
+
       <Nav />
       <Component {...pageProps} />
       <Footer />
