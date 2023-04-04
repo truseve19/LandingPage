dc29f6c5e6917ba6f75cb10236e4924ead05b0ca
Raj kumar <rajkumar@redink.app>
2023-04-04 19:00:32 +0530

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-E63L4H4JT9"
+        src="https://www.googletagmanager.com/gtag/js?id=G-6F8YREHTSD"
         onLoad={() => {
           window.dataLayer = window.dataLayer || []
           function gtag() { dataLayer.push(arguments) }
           gtag('js', new Date())
 
-          gtag('config', 'G-E63L4H4JT9')
+          gtag('config', 'G-6F8YREHTSD')
         }}
       />
 
